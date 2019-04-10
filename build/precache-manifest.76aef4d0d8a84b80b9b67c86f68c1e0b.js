a53f95b6e354b7c731a4b9cafcd20aa5f5f044d0
bratliff <ben@2bitcreative.com>
2019-04-10 19:38:21 -0400

    updated package

---
+++ build/precache-manifest.76aef4d0d8a84b80b9b67c86f68c1e0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8dfa44d367939631ad71",
+    "url": "/static/js/main.2e444ae8.chunk.js"
+  },
+  {
+    "revision": "64044f925cb6f40e12b0",
+    "url": "/static/js/2.53622f50.chunk.js"
+  },
+  {
+    "revision": "8dfa44d367939631ad71",
+    "url": "/static/css/main.4396dc7a.chunk.css"
+  },
+  {
+    "revision": "4257ac64844d3966abb0149b7a818200",
+    "url": "/index.html"
+  }
+];
